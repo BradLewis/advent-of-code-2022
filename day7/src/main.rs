a5f9164af053088b5878b71218bf2591d87b481a
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-02-04 20:33:00 -0500

    Completing day 7 part 1

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,9 +1,10 @@
-use std::fs;
+use std::{cmp::Ordering, fs};
 
 #[derive(Debug)]
 struct Folder {
     subfolders: Vec<Folder>,
     files: Vec<usize>,
+    size: usize,
 }
 
 impl Folder {
@@ -11,24 +12,64 @@ impl Folder {
         Self {
             subfolders: Vec::new(),
             files: Vec::new(),
+            size: 0,
         }
     }
 
-    fn total_size(&self) -> usize {
-        let folders_total: usize = self.subfolders.iter().map(|f| f.total_size()).sum();
-        let files_size: usize = self.files.iter().sum();
-        folders_total + files_size
+    fn from_commands(lines: &Vec<&str>, mut index: usize) -> (Self, usize) {
+        let mut folder = Folder::new();
+        while index < lines.len() {
+            let line = lines[index];
+            index += 1;
+            if line == "$ cd /" || line == "$ ls" {
+                continue;
+            }
+            if line.starts_with("dir") {
+                continue;
+            }
+            if line == "$ cd .." {
+                return (folder, index);
+            }
+            if line.starts_with("$ cd") {
+                let (subfolder, new_index) = Folder::from_commands(lines, index);
+                folder.size += subfolder.size;
+                folder.subfolders.push(subfolder);
+                index = new_index;
+                continue;
+            }
+            let x: Vec<&str> = line.split_whitespace().collect();
+            let size = x[0].parse::<usize>().unwrap();
+            folder.files.push(size);
+            folder.size += size;
+        }
+        (folder, index)
     }
 
-    fn from_string(s: &str) -> Self {
-        Folder::new()
+    fn sum_size_under(&self, limit: usize) -> usize {
+        let mut total_from_subfolders = 0;
+        for subfolder in self.subfolders.iter() {
+            let total = subfolder.sum_size_under(limit);
+            total_from_subfolders += total;
+        }
+        let size = self.size;
+        match size.cmp(&limit) {
+            Ordering::Less => size + total_from_subfolders,
+            _ => total_from_subfolders,
+        }
     }
 }
 
 fn main() {
     let content = fs::read_to_string("input.txt").expect("File not found");
 
-    println!("Hello, world!");
+    part1(&content);
+}
+
+fn part1(s: &str) {
+    let (folder, _) = Folder::from_commands(&s.lines().collect(), 0);
+    let total = folder.sum_size_under(100000);
+
+    println!("{}", total);
 }
 
 #[cfg(test)]
@@ -36,29 +77,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_total_size_files() -> Result<(), String> {
-        let mut folder = Folder::new();
-        folder.files.append(&mut vec![10, 12, 30]);
-        assert_eq!(folder.total_size(), 52);
+    fn test_from_commands() -> Result<(), String> {
+        let s = fs::read_to_string("test_input.txt").expect("File not found");
+        let (folder, _) = Folder::from_commands(&s.lines().collect(), 0);
+        assert_eq!(folder.files.len(), 2);
+        assert_eq!(folder.subfolders.len(), 2);
+        let sum =
+            14848514 + 8504156 + 29116 + 2557 + 62596 + 584 + 4060174 + 8033020 + 5626152 + 7214296;
+        assert_eq!(folder.size, sum);
         Ok(())
     }
 
     #[test]
-    fn test_total_size_folders() -> Result<(), String> {
-        let mut folder = Folder::new();
-        folder.files.append(&mut vec![10, 12]);
-        let mut subfolder = Folder::new();
-        subfolder.files.append(&mut vec![1, 2]);
-        folder.subfolders.push(subfolder);
-        assert_eq!(folder.total_size(), 22 + 3);
-        Ok(())
-    }
-
-    #[test]
-    fn test_from_string() -> Result<(), String> {
-        let mut folder = Folder::new();
-        folder.files.append(&mut vec![10, 12, 30]);
-        assert_eq!(folder.total_size(), 52);
+    fn test_sum_size_under() -> Result<(), String> {
+        let s = fs::read_to_string("test_input.txt").expect("File not found");
+        let (folder, _) = Folder::from_commands(&s.lines().collect(), 0);
+        assert_eq!(folder.sum_size_under(100000), 95437);
         Ok(())
     }
 }
