c183e487fd176dc8cc3170dff2ca2f0978b71297
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-02-13 10:27:27 -0500

    Completing day 12 part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,58 +1,56 @@
 use firestorm::profile_method;
-use std::{
-    collections::{HashMap, HashSet},
-    fs,
-};
+use std::{collections::HashSet, fs};
 
 #[derive(Debug)]
 struct Dijkstra {
-    grid: Vec<Vec<u32>>,
+    height_grid: Vec<Vec<u32>>,
+    grid: Vec<Vec<char>>,
     width: u32,
     height: u32,
     dist: Vec<u32>,
-    prev: HashMap<u32, u32>,
     q: HashSet<u32>,
     start_index: (u32, u32),
-    end_index: (u32, u32),
+    end_value: char,
+    reverse: bool,
 }
 
 impl Dijkstra {
     fn new(s: &str) -> Self {
-        let grid: Vec<Vec<_>> = s
-            .lines()
-            .map(|l| {
-                l.chars()
+        let grid: Vec<Vec<_>> = s.lines().map(|l| l.chars().map(|c| c).collect()).collect();
+        let height_grid: Vec<Vec<_>> = grid
+            .iter()
+            .map(|r| {
+                r.iter()
                     .map(|c| match c {
                         'S' => 0,
                         'E' => 25,
-                        _ => c as u32 - 'a' as u32,
+                        _ => *c as u32 - 'a' as u32,
                     })
                     .collect()
             })
             .collect();
-        let width = grid[0].len() as u32;
-        let height = grid.len() as u32;
+
+        let width = height_grid[0].len() as u32;
+        let height = height_grid.len() as u32;
         let mut start_index = (0, 0);
-        let mut end_index = (0, 0);
         for (y, line) in s.lines().enumerate() {
             for (x, c) in line.chars().enumerate() {
                 if c == 'S' {
                     start_index = (x as u32, y as u32);
-                } else if c == 'E' {
-                    end_index = (x as u32, y as u32);
                 }
             }
         }
 
         Self {
+            height_grid,
             grid,
             width,
             height,
             dist: vec![u32::MAX; (width * height) as usize],
-            prev: HashMap::new(),
             q: HashSet::new(),
             start_index,
-            end_index,
+            end_value: 'E',
+            reverse: false,
         }
     }
 
@@ -71,8 +69,6 @@ impl Dijkstra {
         for y in 0..self.height {
             for x in 0..self.width {
                 let index = self.get_index_value(x, y);
-                self.dist[index as usize] = u32::MAX;
-                self.prev.insert(index, 0);
                 self.q.insert(index);
             }
         }
@@ -87,28 +83,29 @@ impl Dijkstra {
         while !self.q.is_empty() {
             let next_index = self.get_next_vertex();
 
-            let coords = self.get_coords_from_value(next_index);
-            if coords == self.end_index {
+            let (x, y) = self.get_coords_from_value(next_index);
+            if self.grid[y as usize][x as usize] == self.end_value {
                 return self.dist[next_index as usize];
             }
             self.q.remove(&next_index);
-            let neighbours = self.get_valid_neighbours(next_index);
+            let neighbours = match self.reverse {
+                false => self.get_valid_neighbours(next_index),
+                true => self.get_valid_neighbours_reverse(next_index),
+            };
             for neighbour in neighbours.iter() {
                 let new_dist = self.dist[next_index as usize] + 1;
                 if new_dist < self.dist[*neighbour as usize] {
                     self.dist[*neighbour as usize] = new_dist;
-                    self.prev.insert(*neighbour, new_dist);
                 }
             }
         }
-        let (end_x, end_y) = self.end_index;
-        self.dist[self.get_index_value(end_x, end_y) as usize]
+        unreachable!();
     }
 
     fn get_valid_neighbours(&self, index: u32) -> Vec<u32> {
         profile_method!(get_valid_neighbours);
         let (x, y) = self.get_coords_from_value(index);
-        let value = self.grid[y as usize][x as usize];
+        let value = self.height_grid[y as usize][x as usize];
         let to_check: Vec<(i32, i32)> = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
         let mut result: Vec<u32> = Vec::new();
 
@@ -118,7 +115,31 @@ impl Dijkstra {
             if n_x < 0 || n_x as u32 >= self.width || n_y < 0 || n_y as u32 >= self.height {
                 continue;
             }
-            if value + 1 < self.grid[n_y as usize][n_x as usize] {
+            if value + 1 < self.height_grid[n_y as usize][n_x as usize] {
+                continue;
+            }
+            let new_index = self.get_index_value(n_x as u32, n_y as u32);
+            if self.q.contains(&new_index) {
+                result.push(new_index);
+            }
+        }
+        result
+    }
+
+    fn get_valid_neighbours_reverse(&self, index: u32) -> Vec<u32> {
+        profile_method!(get_valid_neighbours_reverse);
+        let (x, y) = self.get_coords_from_value(index);
+        let value = self.height_grid[y as usize][x as usize];
+        let to_check: Vec<(i32, i32)> = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
+        let mut result: Vec<u32> = Vec::new();
+
+        for (i, j) in to_check.iter() {
+            let n_x = x as i32 + i;
+            let n_y = y as i32 + j;
+            if n_x < 0 || n_x as u32 >= self.width || n_y < 0 || n_y as u32 >= self.height {
+                continue;
+            }
+            if value - 1 > self.height_grid[n_y as usize][n_x as usize] {
                 continue;
             }
             let new_index = self.get_index_value(n_x as u32, n_y as u32);
@@ -139,15 +160,38 @@ impl Dijkstra {
             .unwrap();
         min_element
     }
+
+    fn get_index_of(&self, c: char) -> (u32, u32) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                if self.grid[y as usize][x as usize] == c {
+                    return (x, y);
+                }
+            }
+        }
+        unreachable!();
+    }
 }
 
 fn main() {
-    firestorm::bench("./flames/", part1).unwrap();
+    firestorm::bench("./flames/part1/", part1).unwrap();
+    firestorm::bench("./flames/part2/", part2).unwrap();
 }
 
 fn part1() {
     let s = fs::read_to_string("input.txt").expect("File not found");
     let mut d = Dijkstra::new(&s);
+    let result = d.run();
+    println!("{}", result);
+}
+
+fn part2() {
+    let s = fs::read_to_string("input.txt").expect("File not found");
+    let mut d = Dijkstra::new(&s);
+    d.end_value = 'a';
+    d.start_index = d.get_index_of('E');
+    d.reverse = true;
+
     let result = d.run();
     println!("{}", result);
 }
@@ -162,8 +206,8 @@ mod tests {
         let s = fs::read_to_string("test_input.txt").expect("File not found");
 
         let d = Dijkstra::new(&s);
-        assert_eq!(d.grid[0][0], 0);
-        assert_eq!(d.grid[2][4], 25);
+        assert_eq!(d.height_grid[0][0], 0);
+        assert_eq!(d.height_grid[2][4], 25);
         Ok(())
     }
 
@@ -173,7 +217,6 @@ mod tests {
 
         let d = Dijkstra::new(&s);
         assert_eq!(d.start_index, (0, 0));
-        assert_eq!(d.end_index, (5, 2));
 
         Ok(())
     }
@@ -215,6 +258,20 @@ mod tests {
         d.initialise();
 
         assert_eq!(d.get_next_vertex(), 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<(), String> {
+        let s = fs::read_to_string("test_input.txt").expect("File not found");
+
+        let mut d = Dijkstra::new(&s);
+        let start_index = d.get_index_of('E');
+        d.start_index = start_index;
+        d.end_value = 'a';
+        d.reverse = true;
+        let result = d.run();
+        assert_eq!(result, 29);
         Ok(())
     }
 }
