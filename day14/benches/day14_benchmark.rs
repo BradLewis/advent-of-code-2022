2cbf052680399aaf6ee689e4cff62488f74b35af
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-02-17 16:04:31 -0500

    Adding benchmarking to day14

---
+++ day14/benches/day14_benchmark.rs
@@ -0,0 +1,17 @@
+use std::fs;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use day14::{part1, part2};
+
+fn part1_benchmark(c: &mut Criterion) {
+    let s = fs::read_to_string("input.txt").expect("File not found");
+    c.bench_function("part 1", |b| b.iter(|| part1(&s)));
+}
+
+fn part2_benchmark(c: &mut Criterion) {
+    let s = fs::read_to_string("input.txt").expect("File not found");
+    c.bench_function("part 2", |b| b.iter(|| part2(&s)));
+}
+
+criterion_group!(benches, part1_benchmark, part2_benchmark);
+criterion_main!(benches);
