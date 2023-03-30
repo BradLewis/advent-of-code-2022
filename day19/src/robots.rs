fe1efa893c92cab754fdfd2dc9f6dd9a094c3aaf
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-03-30 00:01:36 -0400

    Added parsing for the robots

--- day19/src/robots.rs
+++ day19/src/robots.rs
@@ -1,9 +1,26 @@
+use regex::Regex;
+use std::str::FromStr;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum ResourceType {
     Ore,
     Clay,
     Obsidian,
     Geode,
+}
+
+impl FromStr for ResourceType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "ore" => Ok(ResourceType::Ore),
+            "clay" => Ok(ResourceType::Clay),
+            "obsidian" => Ok(ResourceType::Obsidian),
+            "geode" => Ok(ResourceType::Geode),
+            _ => Err(format!("'{}' is not a valid ResourceType", s)),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -20,11 +37,15 @@ pub struct Robot {
 
 impl Robot {
     pub fn new(s: &str) -> Self {
-        let cost = vec![Price {
-            amount: 0,
-            resource_type: ResourceType::Ore,
-        }];
-        let resource_collected = ResourceType::Ore;
+        let values = parse(s, r"(ore|obsidian|clay|geode|\d+)");
+        let resource_collected = ResourceType::from_str(&values[0]).unwrap();
+        let mut cost: Vec<Price> = Vec::new();
+        for i in (1..values.len()).step_by(2) {
+            cost.push(Price {
+                amount: values[i].parse::<usize>().unwrap(),
+                resource_type: ResourceType::from_str(&values[i + 1]).unwrap(),
+            })
+        }
         Self {
             cost,
             resource_collected,
@@ -32,17 +53,53 @@ impl Robot {
     }
 }
 
+fn parse(s: &str, re: &str) -> Vec<String> {
+    let regex: Regex = Regex::new(re).unwrap();
+    regex
+        .find_iter(s)
+        .map(|m| String::from_str(m.as_str()).unwrap())
+        .collect()
+}
+
 mod tests {
-    use crate::robots::{ResourceType, Robot};
+
+    use crate::robots::parse;
 
     #[test]
-    fn test_create_robot() {
-        let s = String::from("Each ore robot costs 4 ore.");
-        let robot = Robot::new(&s);
+    fn test_regex() {
+        let result = parse(
+            "Each obsidian robot costs 3 ore and 8 clay.",
+            r"(ore|obsidian|clay|geode|\d+)",
+        );
 
-        assert_eq!(robot.resource_collected, ResourceType::Ore);
-        assert_eq!(robot.cost.len(), 1);
-        assert_eq!(robot.cost[0].amount, 4);
-        assert_eq!(robot.cost[0].resource_type, ResourceType::Ore);
+        assert_eq!(result.len(), 5);
+    }
+
+    mod robots {
+        use crate::robots::{ResourceType, Robot};
+
+        #[test]
+        fn test_create_one_cost() {
+            let s = String::from("Each ore robot costs 4 ore.");
+            let robot = Robot::new(&s);
+
+            assert_eq!(robot.resource_collected, ResourceType::Ore);
+            assert_eq!(robot.cost.len(), 1);
+            assert_eq!(robot.cost[0].amount, 4);
+            assert_eq!(robot.cost[0].resource_type, ResourceType::Ore);
+        }
+
+        #[test]
+        fn test_create_two_cost() {
+            let s = String::from("Each obsidian robot costs 3 ore and 8 clay.");
+            let robot = Robot::new(&s);
+
+            assert_eq!(robot.resource_collected, ResourceType::Obsidian);
+            assert_eq!(robot.cost.len(), 2);
+            assert_eq!(robot.cost[0].amount, 3);
+            assert_eq!(robot.cost[0].resource_type, ResourceType::Ore);
+            assert_eq!(robot.cost[1].amount, 8);
+            assert_eq!(robot.cost[1].resource_type, ResourceType::Clay);
+        }
     }
 }
